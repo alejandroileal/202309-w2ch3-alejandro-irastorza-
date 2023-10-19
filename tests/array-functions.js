0ba3811332522a5d4576ddef31735aea93933331
alejandroileal <aleal1202@outlook.com>
2023-10-19 18:38:42 +0200

    Add eslint indent at rules

--- tests/array-functions.js
+++ tests/array-functions.js
@@ -1,46 +1,46 @@
 // Array.length()
 
 export const arrayLength = (array) => {
-	if (!Array.isArray(array)) {
-		throw new Error('Asegúrate de utilizar un Array');
-	}
+  if (!Array.isArray(array)) {
+    throw new Error('Asegúrate de utilizar un Array');
+  }
 
-	let acc = 0;
+  let acc = 0;
 
-	for (const position of array) {
-		acc++;
-	}
+  for (const position of array) {
+    acc++;
+  }
 
-	return acc;
+  return acc;
 };
 
 // Array.push()
 export const arrayPush = (arrayToPush, data) => {
-	arrayToPush[arrayLength(arrayToPush)] = data;
-	const newArrayLength = arrayLength(arrayToPush);
-	return newArrayLength;
+  arrayToPush[arrayLength(arrayToPush)] = data;
+  const newArrayLength = arrayLength(arrayToPush);
+  return newArrayLength;
 };
 
 // Array.unshift()
 
 export const arrayUnshift = (arrayToIterate, item) => {
-	if (Array.isArray(item)) {
-		const unshiftArrayToArray = [...item, ...arrayToIterate];
+  if (Array.isArray(item)) {
+    const unshiftArrayToArray = [...item, ...arrayToIterate];
 
-		return arrayLength(unshiftArrayToArray);
-	}
+    return arrayLength(unshiftArrayToArray);
+  }
 
-	const temporaryArray = [0, ...arrayToIterate];
-	temporaryArray[0] = item;
+  const temporaryArray = [0, ...arrayToIterate];
+  temporaryArray[0] = item;
 
-	return arrayLength(temporaryArray);
+  return arrayLength(temporaryArray);
 };
 
 // Array.pop()
 
 export const arrayPop = (arrayToIterate) => {
-	const lastElementPosition = arrayLength(arrayToIterate) - 1;
-	const lastElement = arrayToIterate[lastElementPosition];
-	console.log(lastElement);
-	return lastElement;
+  const lastElementPosition = arrayLength(arrayToIterate) - 1;
+  const lastElement = arrayToIterate[lastElementPosition];
+  console.log(lastElement);
+  return lastElement;
 };
